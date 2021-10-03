9b65523b2bbba9b2a5a7a2e3b127edf3d46a8547
ssmirnova <svetlana.smirnova213@gmail.com>
2021-10-03 15:37:23 +0200

    Error styles adapted to mobile

--- src/components/error/error.js
+++ src/components/error/error.js
@@ -4,7 +4,9 @@ import './error.scss';
 
 const Error = ({text=''}) => {
     return(
-        <div className="error pt-5">{text}</div>
+        <div className="error pt-5">{text}
+            <img className="d-block error_img" alt={text} src="https://c.tenor.com/6xd1kGTGV-EAAAAC/houston-we-have-a-problem-tom-hanks.gif" />
+        </div>
     )
 }
 
