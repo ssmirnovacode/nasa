ed649b146995305f3e64b80abd5122b8139061ab
ssmirnova <svetlana.smirnova213@gmail.com>
2021-10-03 16:20:13 +0200

    Navmenu collapse on Click bug fixed

--- src/components/navmenu/navmenu.js
+++ src/components/navmenu/navmenu.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
 import { Nav, Navbar, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import './navmenu.scss'
 
 class Navmenu extends Component {
+
+    state = {
+        isDropdownShown: false
+    }
+
+    openDropdown = () => {
+        this.setState({isDropdownShown: true})
+    }
+
+    hideDropdown = () => {
+        this.setState({isDropdownShown: false})
+    }
 
     render() {
 
         return(
-            <Navbar variant="dark" className="navmenu" expand="sm">
+            <Navbar variant="dark" className="navmenu" expand="sm" expanded={this.state.isDropdownShown}>
                 <Container>
-                    <Navbar.Brand>NASA App</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Brand><Link className="fw-bold nav-link navmenu_link" to='/'>NASA App</Link></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={this.openDropdown} />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Link className="nav-link navmenu_link" to='/'>Home</Link>
-                            <Link className="nav-link navmenu_link" to='/earth'>Earth</Link>
-                            <Link className="nav-link navmenu_link" to='/mars'>Mars</Link>
+                            <Link className="nav-link navmenu_link" to='/earth' onClick={this.hideDropdown} >Earth</Link>
+                            <Link className="nav-link navmenu_link" to='/mars' onClick={this.hideDropdown} >Mars</Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
