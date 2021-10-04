b94332e652311e8b05a89fcb294950645f5e1632
ssmirnova <svetlana.smirnova213@gmail.com>
2021-10-04 11:35:31 +0200

    Call effect explained

--- src/redux/sagas/testSaga.js
+++ src/redux/sagas/testSaga.js
@@ -1,10 +1,17 @@
-import { take, put, delay } from 'redux-saga/effects';
+import { take, put, delay, call } from 'redux-saga/effects';
 
+function double(num) {
+    return num*2
+}
 export function* testSaga() {
     while (true) {
-        console.log('Starting saga'); 
-        const state = yield take('TEST MESSAGE'); // returns an object {type: 'TEST MESSAGE', payload: 1000, @@redux-saga/SAGA_ACTION: true}
-        console.log('Finishing saga execution', state); 
+        console.log('Starting saga'); // 1) ... 5)
+        const state = yield take('TEST MESSAGE'); 
+        const a = yield call(double, 2); // calling a function with call
+        console.log(a); // 2) (=4)
+        const b = double(3); // simply calling a function
+        console.log(b); // 3) (=6)
+        console.log('Finishing saga execution', state); // 4)
     }
 }
 
